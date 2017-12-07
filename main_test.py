431655d25309792f1f67c7f685105ea11d95ab43
asubhangi <31353771+asubhangi@users.noreply.github.com>
2017-12-07 00:43:50 -0500

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -15,9 +15,15 @@ class MainTestCase(unittest.TestCase):
 		# propagate the exceptions to the test client
 		self.app.testing = True
 
-	def test_home_status_code(self):
+	'''def test_home_status_code(self):
 		result = self.app.get('/') 
-		self.assertEqual(result.status_code, 200) 
+		self.assertEqual(result.status_code, 200) '''
+	def login(self, username, password):
+	    """Login helper function"""
+	    return main.app.post('/login', data=dict(
+		    username=username,
+		    password=password
+		), follow_redirects=True)
 
 	def test_get_profile(self):
 	    # sends HTTP GET request to the application
@@ -30,18 +36,10 @@ class MainTestCase(unittest.TestCase):
 	def test_get_jobs(self):
 		# sends HTTP GET request to the application
 	    # on the specified path
-	    result = self.app.get('/jobs') 
-
+	    result = self.app.get('/jobs')
 	    # assert the status code of the response
 	    self.assertEqual(result.status_code, 200)
-
-
-	def login(self, username, password):
-	    """Login helper function"""
-	    return main.app.post('/login', data=dict(
-		    username=username,
-		    password=password
-		), follow_redirects=True)
+	
 
 	def logout(self):
 	    """Logout helper function"""
